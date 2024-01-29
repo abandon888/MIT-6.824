2f5d87924cac436b9831041c59cc7e083eca6ff6
Tsingper <tsingper@outlook.com>
2024-01-29 22:46:09 +0800

    feat: add log output file func

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -8,7 +8,11 @@ package raft
 // test with the original before submitting.
 //
 
-import "testing"
+import (
+	"log"
+	"os"
+	"testing"
+)
 import "fmt"
 import "time"
 import "math/rand"
@@ -18,6 +22,20 @@ import "sync"
 // The tester generously allows solutions to complete elections in one second
 // (much more than the paper's range of timeouts).
 const RaftElectionTimeout = 1000 * time.Millisecond
+
+// setupLogging 设置测试的日志输出到文件
+func setupLogging() *log.Logger {
+	// 打开或创建日志文件
+	logFile, err := os.OpenFile("test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("打开日志文件失败: %v", err)
+	}
+
+	// 创建一个新的log.Logger实例
+	testLogger := log.New(logFile, "TEST: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return testLogger
+}
 
 func TestInitialElection2A(t *testing.T) {
 	servers := 3
